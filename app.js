3fcc6312bdcf627300fd6ce5b4b0db465b83ff1d
Alvin Li <alvinl2@me.com>
2018-04-19 22:36:33 -0600

    updated package-lock.json

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const ejs = require('ejs');
+var path = require('path');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+// const path = require('path');
+const mongoose = require('mongoose');
+const ObjectID = require('mongodb').ObjectID;
+const Post = require('./lib/Post')
+
+let session = require('express-session');
+
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+mongoose.connect('mongodb://localhost/test', function(err){
+	if(err){
+		return console.log(err)
+	}
+
+	return console.log("Successfully connected to MongoDB!");
+});
+
+
+var indexRouter = require('./controllers/index');
+// var usersRouter = require('./routes/users');
+
+
+var app = express();
+
+// view engine setup EJS
+app.set('view engine', 'ejs');
+app.use('/discover',express.static('./public'));
+app.use('/',express.static('./views'));
+
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(session({secret:"9aduoshbj1082hd8dowhualj", resave:false, saveUninitialized:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/post',express.static('./views'));
+
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ controllers/index.js
@@ -0,0 +1,363 @@
+// var express = require('express');
+
+var mongoose = require('mongoose');
+var User = require('../lib/User');
+
+const express = require('express');
+
+var router = express.Router();
+
+const ejs = require('ejs');
+var path = require('path');
+// const bodyParser = require('body-parser');
+const multer = require('multer');
+// const path = require('path');
+// const mongoose = require('mongoose');
+const ObjectID = require('mongodb').ObjectID;
+const Post = require('../lib/Post');
+const Comment = require('../lib/Comment');
+var ObjectId = require('mongodb').ObjectID;
+// const waveSurf = require('../lib/fileHandler.js');
+// const moment = require('moment');
+
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//     res.render('index', { title: 'Express' });
+// });
+
+// Retrieve Log In Page
+router.get('/login', function(req,res){
+  res.render('login');
+})
+
+
+
+router.get('/getcomments',function(req,res){
+
+
+  let queryComments = Comment.find();
+  queryComments.find({'post_id':req.c});
+
+  queryComments.exec(function(err,comments){
+    if(err){
+      res.render('index',{
+        msg: err
+      });
+    }
+    else{
+      res.send(comments);
+    }
+  });
+
+});
+
+
+// Submit Login Credentials
+router.post('/login', function(req, res){
+    console.log(req);
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log(username);
+    console.log(password);
+
+    User.findOne({username: username, password: password}, function(err, user){
+
+        if(err){
+            console.log(err);
+            return res.status(500).send();
+        }
+        else if(user==null){
+            // return res.status(404).send();
+            return res.render('login');
+        }else{
+
+        req.session.user = user;
+        return res.render('index');
+      }
+    });
+
+});
+
+router.get('/dashboard', function(req, res){
+    if(!req.session.user){
+        //return res.status(401).send();
+        return res.redirect('/login');
+    }
+
+    return res.status(200).send("Welcome");
+})
+
+router.get('/logout', function(req, res){
+    req.session.destroy();
+    return res.status(200).send();
+})
+
+// Retrieve registration page
+router.get('/register', function(req, res){
+  res.render('signup');
+});
+
+
+// Submit registration request
+router.post('/register', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log(username);
+    console.log(password);
+
+    var newUser = new User();
+    newUser.username = username;
+    newUser.password = password;
+
+    newUser.save(function(err, savedUser){
+        if(err){
+            console.log(err);
+            // return res.status(500).send();
+        }
+        return res.render('login');
+    })
+})
+
+
+// Alvin's stuff below--------
+
+// Set Storage engine
+let storage = multer.diskStorage({
+  destination: './public/',
+  filename: function(reg,file,cb){
+    cb(null,file.myImage+'-'+Date.now()+path.extname(file.originalname));
+    console.log(path.extname(file.originalname));
+    // cb(null,file.myImage+'-'+Date.now()+'.jpg');
+  }
+});
+
+// parser for uploads via Multer
+const upload = multer({
+  storage: storage,
+  // fileFilter:function(req,file,cb){
+  //   checkFileType(file,cb);
+  // }
+// }).single('myWave');
+
+}).single('image_file');
+
+
+
+// Check File type
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpg|jpeg|png/;
+  // Check extn
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(extname&&extname){
+    return cb(null,true);
+  }
+  else{
+    cb('Error: Images Only!');
+  }
+}
+
+
+// Authenicate the user and password to match a DB entry
+function authenticateCredentials(req,res){
+  return function(req,res,next){
+      next();
+  }
+}
+
+// Check if user is logged in
+function checkIfLoggedIn(){
+  return function(req,res,next){
+      // console.log(req.session.user);
+      // if(req.session.user!=undefined){
+      //     next();
+      // }
+      // else{
+      //   console.log("NOT LOGGED IN!!");
+      //   res.render('index0');
+      // }
+
+      next();
+  }
+}
+
+// loads login page
+// router.post('/login',authenticateCredentials(),function(req,res){
+//   // res.render('home');
+// });
+
+// Renders the main page if user is logged in.
+router.get('/',checkIfLoggedIn(),(req,res)=>res.render('index'));
+
+
+// Render Upload page
+router.get('/upload',function(req,res){
+  res.render('upload');
+});
+
+// Upload a file to server and registers it as a post in DB
+router.post('/upload',(req,res)=>{
+  // console.log(req);
+	console.log("upload request");
+  // create post via post Schema
+  upload(req,res,(err)=>{
+    // console.log(req.file.filename);
+    console.log(req.body.title);
+    // console.log('-----------------------------');
+    // console.log(req);
+    if(err){
+      console.log("failed")
+      res.render('index',{
+        msg: err
+      });
+    }
+    else
+    {
+      if(req.file==undefined){
+        console.log("Error: No File Selected!'");
+        res.render('index',{
+          msg: 'Error: No File Selected!'
+        });
+      }
+      else{
+        res.render('index',{
+          msg: 'File Uploaded!',
+          // file: `${req.file.filename}`
+        });
+
+        let post = new Post();
+        let title = req.title;
+
+          post.filepath = req.file.filename;
+          post.post_id = req.file.filename;
+          post.author = req.body.author;
+          post.title = req.body.title;
+          post.date = Date.now();
+
+          post.save(function(err){
+            if(err){
+              console.log("FAILED");
+              console.log(err);
+              return res.status(500).send();
+            }
+            else{
+              console.log("sent to db");
+              console.log(post);
+              return res.status(200).send();
+            }
+          });
+
+      }
+    }
+  }
+);
+
+
+
+
+});
+
+
+// Retrieves the server filepath of a file based on post_id and displays it
+router.get('/post/:_id',(req,res)=>{
+  console.log('generating post');
+
+  let query = Post.findOne({'_id':ObjectID(req.params._id)});
+  query.select('_id filepath title date');
+
+  query.exec(function(err,post){
+    if(err){
+      res.render('index',{
+        msg: err
+      });
+    }
+    else{
+      // console.log(post._id);
+
+      res.render('post',{
+        file: post,
+        filetype: 'jpg',
+        // comments: comments;
+      });
+    }
+  });
+
+
+});
+
+
+// Post Comment
+// Add a comment to an existing post.
+router.post('/comment',(req,res)=>{
+  let comment = new Comment();
+
+  let link = req.headers.referer;
+  let parsed = link.split('/');
+  let id = parsed[parsed.length-1];
+  comment.post_id = id;
+  console.log(id);
+  comment.comment = req.body.comment;
+  // comment.author = req.session.user.username;
+  // comment.post_id = req;
+  comment.date = Date.now();
+
+  comment.save(function(err){
+    if(err){
+      console.log("FAILED");
+      console.log(err);
+      // return res.status(500).send();
+    }
+    else{
+      console.log("sent to db");
+      // console.log(post);
+      // res.status(200).send({
+      //   comment: comment
+      // });
+      // console.log(req);
+      // res.redirect('/post/undefined-1523845557148.png',{
+      //   comment: comment
+      // });
+
+      let link = req.headers.referer;
+
+      res.redirect(link);
+
+    }
+  });
+});
+
+
+
+router.get('/test',function(req,res){
+  console.log('console log of the test.');
+  res.send('hi, this is a test!');
+});
+
+
+// Displays all posts uploaded
+router.get('/discover',function(req,res){
+  let query = Post.find();
+  query.select('post_id');
+
+  query.exec(function(err,posts){
+    if(err){
+      res.render('index',{
+        msg: err
+      });
+    }
+    else{
+      // console.log(posts);
+
+      res.render('discover',{
+        posts: posts,
+        filetype: 'png'
+      });
+    }
+  });
+});
+
+module.exports = router;

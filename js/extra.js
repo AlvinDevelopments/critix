9fbaffb702f0a700d46dcf9ebe4e1ed5a39459ee
ejaguete <erica.aguete@gmail.com>
2018-04-20 23:33:16 -0600

    fixed mobile navbar (before it was greyed out and links wouldnt work)

--- js/extra.js
+++ js/extra.js
@@ -1,29 +1,28 @@
 // contains client javascript
+loggedIn = false;
+
+// TODO: function to read cookies and change navbar based on if user is logged in
+function navLinks() {
+    let signup = '<li><a href="signup.html">Sign up</a></li>';
+    let login = '<li><a href="login.html">Login</a></li>';
+    let upload = '<li><a href="#">Upload</a></li>';
+    let profile = '<li><a href="#">Profile</a></li>';
+    let logout = '<li><a href="#">Logout</a></li>';
+    // clear links
+    $('#nav-mobile').empty();
+    $('#mobilesidenav').empty();
+
+    if(!loggedIn) {
+        $('#nav-mobile').append(signup, login);
+        $('#mobilesidenav').append(signup, login);
+    } else {
+        $('#nav-mobile').append(upload, profile, logout);
+        $('#mobilesidenav').append(upload,profile,logout);
+    }
+}
 
 // toggle sidebar menu on mobile
 $(document).ready(function(){
     $('.sidenav').sidenav();
 });
 
-loggedIn = false;
-
-// TODO: function to read cookies and change navbar based on if user is logged in
-function navLinks() {
-    // clear links
-    $('#nav-mobile').empty();
-    $('#mobilesidenav').empty();
-
-    if(!loggedIn) {
-        $('#nav-mobile').append('<li><a href="signup.html">Sign up</a></li>');
-        $('#nav-mobile').append('<li><a href="login.html">Login</a></li>');
-        $('#mobilesidenav').append('<li><a href="signup.html">Sign up</a></li>');
-        $('#mobilesidenav').append('<li><a href="login.html">Login</a></li>');
-    } else {
-        $('#nav-mobile').append('<li><a href="#">Upload</a></li>');
-        $('#nav-mobile').append('<li><a href="#">Profile</a></li>');
-        $('#nav-mobile').append('<li><a href="#">Logout</a></li>');
-        $('#mobilesidenav').append('<li><a href="#">Upload</a></li>');
-        $('#mobilesidenav').append('<li><a href="#">Profile</a></li>');
-        $('#mobilesidenav').append('<li><a href="#">Logout</a></li>');
-    }
-}
